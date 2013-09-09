9993adbc503cee380447be902646e3f1aade1bae
LukeBarr <lukebarr@hotmail.co.uk>
2013-09-09 13:12:03 +0100

    Added Splash graphics
    Image fades in and out, run the project see what you think. You dont
    need android device just run the Main.java thats in the desktop src. We
    could add a logo here later or something. We should get working on the
    basic main menu soon and then finally gameplay (shudders)

--- IntoDarkeness/Into-Darkness/src/com/duck/intdark/screens/Splash.java
+++ IntoDarkeness/Into-Darkness/src/com/duck/intdark/screens/Splash.java
@@ -1,20 +1,27 @@
 package com.duck.intdark.screens;
 
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenManager;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.duck.intdark.tween.SpriteAccessor;
 
 public class Splash implements Screen {
 	private Sprite splash;
 	private SpriteBatch batch;
+	private Texture splashTexture;
+	private TweenManager tweenManager;
 
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		tweenManager.update(delta);
 
 		batch.begin();
 		splash.draw(batch);
@@ -29,10 +36,19 @@ public class Splash implements Screen {
 	@Override
 	public void show() {
 		batch = new SpriteBatch();
+		tweenManager = new TweenManager();
+		Tween.registerAccessor(Sprite.class, new SpriteAccessor());
 
-		Texture splashTexture = new Texture("data/splash.png");
+		splashTexture = new Texture("data/splash.png");
+		splashTexture.setEnforcePotImages(false);
 		splash = new Sprite(splashTexture);
 		splash.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		
+		Tween.set(splash, SpriteAccessor.ALPHA).target(0).start(tweenManager);
+		Tween.to(splash, SpriteAccessor.ALPHA, 3).target(1).start(tweenManager);
+		
+		Tween.to(splash, SpriteAccessor.ALPHA, 5).target(0).delay(3).start(tweenManager);
 
 	}
 
@@ -53,7 +69,8 @@ public class Splash implements Screen {
 
 	@Override
 	public void dispose() {
-
+		batch.dispose();
+		splashTexture.dispose();
 	}
 
 }
