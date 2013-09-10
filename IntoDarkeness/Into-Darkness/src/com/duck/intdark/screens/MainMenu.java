173365b34712ab5e1d4029a4561cb04de7795d9f
Conor Flynn <conor@conors-laptop.home-network>
2013-09-10 15:45:49 +0100

    Committed broken MainMenu.java. Layout/Skins need to be created to fix.

--- IntoDarkeness/Into-Darkness/src/com/duck/intdark/screens/MainMenu.java
+++ IntoDarkeness/Into-Darkness/src/com/duck/intdark/screens/MainMenu.java
@@ -9,21 +9,24 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class MainMenu implements Screen {
 	private Sprite test;
-	private SpriteBatch testbatch;
-	private Texture testt;
+	private SpriteBatch test_batch;
+	private Texture test_texture;
 	private Stage stage;
 	private Table table;
-	private TextButton buttonPlay,buttonExit;
+	private TextButton buttonPlay, buttonExit;
+	
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		testbatch.begin();
-		test.draw(testbatch);
-		testbatch.end();
+		test_batch.begin();
+		test.draw(test_batch);
+		test_batch.end();
 
 	}
 
@@ -34,11 +37,34 @@ public class MainMenu implements Screen {
 
 	@Override
 	public void show() {
-		testt = new Texture("data/splash.png");
-		test = new Sprite(testt);
+		test_texture = new Texture("data/splash.png"); //TODO: Menu image
+		test = new Sprite(test_texture);
 		test.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		testbatch = new SpriteBatch();
+		test_batch = new SpriteBatch();
 
+		//TODO: Skins, etc... See http://steigert.blogspot.ie/2012/03/4-libgdx-tutorial-tablelayout.html
+		table = new Table();
+		TableLayout layout = table.getTableLayout();
+
+
+		//Register buttonPlay and buttonExit
+		buttonPlay = new TextButton("Play!", new TextButton.TextButtonStyle());
+		buttonPlay.setClickListener( new ClickListener() {
+			@Override
+			public void click(Actor actor, float x, float y) {
+				//TODO: game.setScreen();
+			}
+		} );
+		layout.register("buttonPlay", buttonPlay);
+
+		buttonExit = new TextButton("Exit", new TextButton.TextButtonStyle());
+		buttonExit.setClickListener( new ClickListener() {
+			@Override
+			public void click(Actor actor, float x, float y) {
+				//TODO: game.setScreen();
+			}
+		});
+		layout.register("buttonExit", buttonExit);
 	}
 
 	@Override
@@ -58,7 +84,7 @@ public class MainMenu implements Screen {
 
 	@Override
 	public void dispose() {
-
+		//TODO: Dispose objects created...
 	}
 
 }
