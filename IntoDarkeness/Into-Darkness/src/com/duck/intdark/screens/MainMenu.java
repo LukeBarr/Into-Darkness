6714dd0bdb5757e3dde3bdfc8eb5499fe8efe198
LukeBarr <lukebarr@hotmail.co.uk>
2013-09-16 13:55:09 +0100

    Rectangle sprite button method added
    Not too sure why it is not working correctly, haven't had much time to
    look at the code but I can't see why it isn't working. Maybe a bug in
    the .getBoundingRectangle() method not sure.

--- IntoDarkeness/Into-Darkness/src/com/duck/intdark/screens/MainMenu.java
+++ IntoDarkeness/Into-Darkness/src/com/duck/intdark/screens/MainMenu.java
@@ -2,45 +2,39 @@ package com.duck.intdark.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.math.Rectangle;
 
 public class MainMenu implements Screen {
-	private Sprite test;
-	private SpriteBatch test_batch;
-	private Texture test_texture;
-	private Stage stage;
-	private TextureAtlas atlas;
-	private Table table;
-	private TextButton buttonPlay, buttonExit;
-	private BitmapFont white;
-	private Label heading;
-	private Skin skin;
-
+	private SpriteBatch batch;
+	private Sprite buttonPlay;
+	private Texture playTexture;
+	private Rectangle playRec;
+	private boolean test;
+	private  int HEIGHT,WIDTH;
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		test_batch.begin();
-		test.draw(test_batch); //Background Image
-		test_batch.end();
-		
-		
-		stage.act(delta);
 
-		stage.draw();
+		if (Gdx.input.isTouched()) {
+			if (playRec.contains(Gdx.input.getX(), Gdx.input.getY())) {
+				test = false;
+				// Next screen
+
+			}
+
+		}
+
+		batch.begin();
+		if (test)
+			buttonPlay.draw(batch);
+
+		batch.end();
 
 	}
 
@@ -51,40 +45,20 @@ public class MainMenu implements Screen {
 
 	@Override
 	public void show() {
-		test_texture = new Texture("data/splash.png"); // TODO: Menu image
-		test = new Sprite(test_texture);
-		test.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		test_batch = new SpriteBatch();
+		HEIGHT=Gdx.graphics.getHeight();
+		WIDTH=Gdx.graphics.getWidth();
+		test = true;
+		batch = new SpriteBatch();
+		playTexture = new Texture("ui/play.png");
 
-		stage = new Stage();
-		white = new BitmapFont(Gdx.files.internal("font/abbeyFont.fnt"), false); //See BMFont to create fonts
+		buttonPlay = new Sprite(playTexture);
+		buttonPlay.setBounds(
+				WIDTH / 2 - buttonPlay.getWidth() / 2,
+				HEIGHT - HEIGHT / 3,
+				WIDTH - WIDTH / 2,
+				HEIGHT / 10);
 
-		
-		Gdx.input.setInputProcessor(stage); //Stage reads inputs
-		
-		
-		
-		
-		
-		
-		atlas = new TextureAtlas("ui/button.pack"); //See Libgdx texturepacker
-		skin = new Skin(atlas);
-		table = new Table(skin);
-		table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		TextButtonStyle textButtonStyle = new TextButtonStyle();
-		textButtonStyle.up = skin.getDrawable("button.up");
-		textButtonStyle.down = skin.getDrawable("button.down.9");
-		textButtonStyle.pressedOffsetX = 1;
-		textButtonStyle.pressedOffsetY = 1;
-		textButtonStyle.font = white;
-		textButtonStyle.fontColor = Color.BLACK;
-
-		buttonPlay = new TextButton("Play", textButtonStyle);
-		buttonPlay.pad(20);
-
-		table.add(buttonPlay);
-		stage.addActor(table);
-		
+		playRec = new Rectangle(buttonPlay.getBoundingRectangle());
 
 	}
 
@@ -105,11 +79,8 @@ public class MainMenu implements Screen {
 
 	@Override
 	public void dispose() {
-		test_batch.dispose();
-		test_texture.dispose();
-		stage.dispose();
-		skin.dispose();
-		
+		batch.dispose();
+		playTexture.dispose();
 
 	}
 
